9415202d19dfa107eca1469184e62fc16dc758d1
jolichh <113618956+jolichh@users.noreply.github.com>
2024-03-06 19:04:38 +0100

    intentando cambiar nombre del proyecto

--- TIC_BCN_JJLC/TIC_BCN_JJLC/settings.py
+++ TIC_BCN_JJLC/TIC_BCN_JJLC/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # nombreApp, fichero.py, clase(o metodo)
+    'centre.apps.CentreConfig',
 ]
 
 MIDDLEWARE = [
