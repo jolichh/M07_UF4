694fdd84fd7c0823dd58df575bbe152a5660f632
jolichh <113618956+jolichh@users.noreply.github.com>
2024-03-08 17:04:02 +0100

    accede a un index dentro del template centre

--- TIC_BCN_JJLC/centre/views.py
+++ TIC_BCN_JJLC/centre/views.py
@@ -9,7 +9,21 @@ from django.template import Context, loader
 #     dades = template.render({'nombre': professor["name"], 'age': professor["age"]})
 #     return HttpResponse(dades)
 
-def index(request):
-    professor = {"name":"Roger","surname":"Sobrino", "age":"18"}
+# def index(request):
+#     professor = {"name":"Roger","surname":"Sobrino", "age":"18"}
     
-    return render(request, 'index_centre.html', {'nombre': professor["name"], 'age': professor["age"]})
+#     return render(request, 'index_centre.html', {'nombre': professor["name"], 'age': professor["age"]})
+
+def index(request):     
+    template = loader.get_template('index.html')   
+    return HttpResponse(template.render())
+
+def alumnos(request):
+    alum = [{"name":"Joana","surname":"Lin", "age":"18"}]
+    context = {'alumnos': alum}
+    return render(request, 'alum.html', context)
+
+def profes(request):
+    profes = [{"name":"Roger","surname":"Sobrino", "email":"rsobringo@itic.cat", "curs":"DAW i DAM"}]
+    context = {'profe':profes}
+    return render(request, 'prof.html', context)
