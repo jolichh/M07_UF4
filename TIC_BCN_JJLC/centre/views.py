81ea93914f529a86a8ca210f54585e8a2bccffbd
jolichh <113618956+jolichh@users.noreply.github.com>
2024-03-08 15:59:38 +0100

    poniendo headers

--- TIC_BCN_JJLC/centre/views.py
+++ TIC_BCN_JJLC/centre/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader 
 
 # Create your views here.
+# def index(request):
+#     professor = {"name":"Roger","surname":"Sobrino", "age":"17"}
+#     template = loader.get_template('index_centre.html')
+#     dades = template.render({'nombre': professor["name"], 'age': professor["age"]})
+#     return HttpResponse(dades)
+
+def index(request):
+    professor = {"name":"Roger","surname":"Sobrino", "age":"18"}
+    
+    return render(request, 'index_centre.html', {'nombre': professor["name"], 'age': professor["age"]})
