912aac12e57a4f0553f50778ec5bd62d4d0f38f9
jolichh <113618956+jolichh@users.noreply.github.com>
2024-03-17 19:39:24 +0100

    creacio conectar bbdd

--- TIC_BCN_JJLC/TIC_BCN_JJLC/settings.py
+++ TIC_BCN_JJLC/TIC_BCN_JJLC/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'TIC_BCN_JJLC.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'ladatabase',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
